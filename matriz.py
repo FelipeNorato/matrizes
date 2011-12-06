d1eed4427c7dcaf188645ad1f3e01ea511e098ca
Felipe Norato Lacerda <norato.guitar@gmail.com>
2011-12-06 03:31:08 -0200

    Matrix LU covered 100% test

--- matriz.py
+++ matriz.py
@@ -14,7 +14,7 @@ class Matriz(object):
     def __eq__(self, representacao_matriz):
         return self.representacao_matriz == representacao_matriz
 
-    def verificar_matriz(self):
+    def _verificar_matriz(self):
         pass
 
     @property
@@ -28,9 +28,9 @@ class Matriz(object):
                     a[linha][coluna]=valor, (a11=1, a12=3..)
         """
         for linha, coluna in self.todas_as_posicoes:
-            posicao = ''.join(map(str,['a', linha, coluna]))
+            posicao = ''.join(map(str,['a' , linha, coluna]))
             self[linha - 1].append(kwargs[posicao])
-        self.verificar_matriz()
+        self._verificar_matriz()
         return self
 
     def termo_independente(self, **kwargs):
@@ -44,7 +44,7 @@ class Matriz(object):
 
 class Superior(Matriz):
 
-    def verificar_matriz(self):
+    def _verificar_matriz(self):
         for linha, coluna in self.todas_as_posicoes:
             if linha > coluna and self[linha-1][coluna-1] != 0:
                 raise MatrixError("A Matriz instanciada nao e Triangular Superior")
@@ -57,7 +57,7 @@ class Superior(Matriz):
                     somatorio +=0
                 else:
                     somatorio += self[linha -1 ][coluna -1] * self._value[coluna -1]
-            resultado = (self.repr_termos[linha - 1] - somatorio) / self[linha - 1][linha -1] 
+            resultado = round((self.repr_termos[linha - 1] - somatorio) / self[linha - 1][linha -1],3) 
             somatorio = 0
             self.result_x.insert(0, resultado)
             self._value[linha -1] = resultado
@@ -65,7 +65,7 @@ class Superior(Matriz):
 
 class Inferior(Matriz):
 
-    def verificar_matriz(self):
+    def _verificar_matriz(self):
         for linha, coluna in self.todas_as_posicoes:
             if linha < coluna and self[linha-1][coluna-1] != 0:
                 raise MatrixError("A Matriz instanciada nao e Triangular Inferior")
@@ -78,10 +78,51 @@ class Inferior(Matriz):
                     somatorio +=0
                 else:
                     somatorio += self[linha-1][coluna-1] * self.result_x[coluna - 1]
-            resultado = (self.repr_termos[linha -1] + somatorio) /self[linha -1][linha -1]
+            resultado = round((self.repr_termos[linha -1] + (somatorio * -1)) /self[linha -1][linha -1],3)
             self.result_x.append(resultado) 
             somatorio = 0
         return self.result_x
+
+class LU(Matriz):
+
+    def _verificar_matriz(self):
+        pass
+    def _decompor(self):
+        matriz_l = Inferior(ordem=self.ordem)
+        matriz_u = Superior(ordem=self.ordem)
+        lista_u = {} 
+        lista_l = {}
+        somatorio = 0
+        pos = lambda p,i,j:"%s%i%i"%(p,i,j)
+        for linha, coluna in self.todas_as_posicoes:
+            if linha > coluna:
+                somatorio = 0
+                for k in range(1,coluna):
+                    somatorio += lista_l[pos('a',linha,k)] * lista_u[pos('a',k,coluna)]
+                lista_l[pos('a',linha,coluna)] = round((1./lista_u[pos('a',coluna,coluna)]) * (self[linha-1][coluna-1] - somatorio),3)
+                lista_u[pos('a',linha,coluna)] = 0
+            elif linha <= coluna:
+                somatorio = 0
+                for k in range(1,linha):
+                    somatorio += (lista_l[pos('a',linha,k)] * lista_u[pos('a',k,coluna)])
+                lista_u[pos('a',linha,coluna)] = round(self[linha-1][coluna-1] - somatorio,3)
+                if linha == coluna:
+                    lista_l[pos('a',linha,coluna)] = 1
+                else:
+                    lista_l[pos('a',linha,coluna)] = 0
+        matriz_u.set_matriz(**lista_u)
+        matriz_l.set_matriz(**lista_l)
+        return matriz_u.representacao_matriz,matriz_l.representacao_matriz
+
+
+    def calcular(self):
+        matriz_u = Superior(ordem=self.ordem)
+        matriz_l = Inferior(ordem=self.ordem)
+        matriz_u.representacao_matriz = self._decompor()[0]
+        matriz_l.representacao_matriz = self._decompor()[1]
+        matriz_l.repr_termos = self.repr_termos
+        matriz_u.repr_termos = matriz_l.calcular()
+        return matriz_u.calcular()
 
 class MatrixError(Exception):
     pass
--- matriz_spec.py
+++ matriz_spec.py
@@ -1,6 +1,6 @@
 import unittest
 from should_dsl import should
-from matriz import Matriz, Superior, Inferior, MatrixError
+from matriz import Matriz, Superior, Inferior, LU, MatrixError
 
 
 class TestInstanciarMatriz(unittest.TestCase):
@@ -44,6 +44,15 @@ class TestMatrizTriangularSuperior(unittest.TestCase):
         self.superior.termo_independente(b1=10, b2=0, b3=5)
         self.superior.calcular() |should| equal_to([7,2,1])
 
+    def test_matriz_exemplo_LU(self):
+        self.superior = Superior(ordem=3)
+        self.superior.set_matriz(a11=3, a12= 2,    a13=4, 
+                                 a21=0, a22= 0.334, a23=0.667, 
+                                 a31=0, a32= 0,    a33=-8) 
+
+        self.superior.termo_independente(b1=1, b2=1.667, b3=0)
+        self.superior.calcular() |should| equal_to([-2.994, 4.991, 0.0])
+
     def test_erro_matriz_nao_triangular_superior(self):
         self.superior = Superior(ordem=3)
         (lambda: \
@@ -64,15 +73,25 @@ class TestMatrizTriangularInferior(unittest.TestCase):
         self.inferior.termo_independente(b1=2 ,b2=-3 , b3=1 )
         self.inferior.calcular() |should| equal_to([1, -1, 1])
 
-    def test_calculo_matriz_triangular_inferior_inventada(self):
+
+    def test_matriz_exemplo_LU(self):
         self.inferior = Inferior(ordem=3)
-        self.inferior.set_matriz(a11=-2 ,a12=0 ,a13=0,
-                                 a21= 3 ,a22=1 ,a23=0,
-                                 a31= 2 ,a32=1 ,a33=2)
+        self.inferior.set_matriz(a11=1 ,a12=0 ,a13=0,
+                                 a21=0.334 ,a22=1 ,a23=0,
+                                 a31=1.334 ,a32=1 ,a33=1)
 
-        self.inferior.termo_independente(b1=2 ,b2=-3 , b3=1 )
-        self.inferior.calcular() |should| equal_to([-1, -6, -4])
+        self.inferior.termo_independente(b1=1 ,b2=2 , b3=3 )
+        self.inferior.calcular() |should| equal_to([1, 1.666, 0.0])
 
+    def test_matriz_du_jaozin(self):
+        self.inferior = Inferior(ordem=4)
+        self.inferior.set_matriz(a11=2, a12=0, a13=0, a14=0,
+                                 a21=2, a22=4, a23=0, a24=0,
+                                 a31=7, a32=3, a33=4, a34=0,
+                                 a41=1, a42=4, a43=5, a44=3)
+
+        self.inferior.termo_independente(b1=4, b2=24, b3=33, b4=36)
+        self.inferior.calcular() |should| equal_to([2,5,1,3])
     def test_erro_matriz_nao_triangular_inferior(self):
         self.inferior = Inferior(ordem=3)
         (lambda:
@@ -81,3 +100,23 @@ class TestMatrizTriangularInferior(unittest.TestCase):
                                  a31= 2 ,a32=1 ,a33=2))\
                                  |should| throw(MatrixError, message="A Matriz instanciada nao e Triangular Inferior")
 
+class TestMatrizDecomposicaoLUOrdem3(unittest.TestCase):
+
+
+    def setUp(self):
+        self.lu = LU(ordem=3)
+        self.lu.termo_independente(b1=1 ,b2=2 , b3=3)
+        self.lu.set_matriz(a11=3, a12=2, a13=4, 
+                           a21=1, a22=1, a23=2,
+                           a31=4, a32=3, a33=-2)
+
+    def test_decompor_matriz_LU_superior(self):
+        self.lu._decompor()[0] |should| equal_to(([[3.0 ,2.0   ,4.0], 
+                                                  [0    ,0.334 ,0.668], 
+                                                  [0    ,0     ,-8.0]])) 
+    def test_decompor_matriz_LU_inferior(self):
+        self.lu._decompor()[1] |should| equal_to([[1     ,0 ,0],
+                                                 [0.333 ,1 ,0],
+                                                 [1.333 ,1 ,1]])
+    def test_calculo_LU(self):
+        self.lu.calcular() |should| equal_to([-2.994, 4.991, 0.0])
